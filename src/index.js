64a482d776494232d4daef578d12974146076955
svshyam91 <svshyam111@gmail.com>
2020-09-28 13:37:40 +0530

    Use of Function based components

--- src/index.js
+++ src/index.js
@@ -2,29 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-class Person extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      'hobbies': "Programming, Cricket",
-    }
-  }
-
-  addHobbies = () => {
-    this.setState(
-      {'hobbies': 'Programming, Cricket, Music'}
-    )
-  }
-
-  render() {
-    return(
-      <div>
-        <p>My name is {this.props.name} and my age is {this.props.age}</p>
-        <p>Hobbies: {this.state.hobbies}</p>
-        <button onClick={this.addHobbies}>Other Hobbies</button>
-      </div>
-    )
-  }
-}
+// Function based component 
+const Person = (props) => {
+  return(
+    <div>
+      <p>My name is {props.name} and my age is {props.age}. </p>
+    </div>
+  )
+} 
 
 ReactDOM.render(<Person name="Shyam" age="23" />, document.getElementById('root'));
