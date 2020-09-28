03237c2ac929d594d72e6830441b5d60739ec7fa
svshyam91 <svshyam111@gmail.com>
2020-09-28 13:14:05 +0530

    Use of props

--- src/index.js
+++ src/index.js
@@ -3,29 +3,10 @@ import ReactDOM from 'react-dom'
 
 
 class Person extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-      color: "red"
-    }
-  }
-
-  changeColor = () => {
-    this.setState({color: "yellow"})
-  }
-  
-  static getDerivedStateFromProps = (props, state) => {
-    // this.setState({color: "yellow"});
-    // 'this' inside static function will not work because static functions are not associate with any instance of class
-    return {color: "blue"}
-  }
-
   render() {
     return(
-      <div style = {{color: this.state.color }}>
-        <p>My name is {this.props.name}</p>
-        <p>My age is {this.props.age} </p>
-        <button onClick={this.changeColor}>Change Color</button>
+      <div>
+        <p>My name is {this.props.name} and my age is {this.props.age}</p>
       </div>
     )
   }
