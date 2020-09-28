e564613e81aac1cd403729f34bc6585a2c3d8f7f
svshyam91 <svshyam111@gmail.com>
2020-09-28 13:19:31 +0530

    Use of commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom'
 
 
 class Person extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'hobbies': ['programming', 'cricket']
+    }
+  }
   render() {
     return(
       <div>
         <p>My name is {this.props.name} and my age is {this.props.age}</p>
+        <p>Hobbies: {this.state.hobbies[0]} and {this.state.hobbies[1]}</p>
       </div>
     )
   }
