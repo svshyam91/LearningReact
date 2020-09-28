822fa574afbbcf85d5b97796271938d45000e8ec
svshyam91 <svshyam111@gmail.com>
2020-09-28 14:31:53 +0530

    Use of state in function based component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
-
 
 // Function based component 
 const Person = (props) => {
+
+  // 'useState()' returns array of 2 elements in which first element is state and 
+  // second element is function to change that state.
+  const [personState, setPersonState] = useState({
+    name: "Shyam Kumar", 
+    age: 23,
+  });
+
+  const [hobbiesState, setHobbiesState] = useState({
+    hobbies: 'Programming, Cricket',
+  })
+
+  // Change state 
+  const changeNameHandler = () => {
+    setPersonState({
+      name: "Mahindra Singh Dhoni",
+      age: 38
+    });
+
+    setHobbiesState({
+      hobbies: 'Programming, Cricket, Music'
+    });
+  }
+
   return(
     <div>
-      <p>My name is {props.name} and my age is {props.age}. </p>
+      <p>My name is {personState.name} and my age is {personState.age}. </p>
+      <p>My hobbies are: {hobbiesState.hobbies}</p>
+      <button onClick={changeNameHandler}>Change Name</button>
     </div>
   )
 } 
