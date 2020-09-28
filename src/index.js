566d54aac3996ea284a372e1daab629e2b7dfb31
voyager97 <svshyam111@gmail.com>
2020-09-28 12:48:15 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Person extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      color: "red"
+    }
+  }
+
+  changeColor = () => {
+    this.setState({color: "yellow"})
+  }
+  
+  static getDerivedStateFromProps = (props, state) => {
+    // this.setState({color: "yellow"});
+    // 'this' inside static function will not work because static functions are not associate with any instance of class
+    return {color: "blue"}
+  }
+
+  render() {
+    return(
+      <div style = {{color: this.state.color }}>
+        <p>My name is {this.props.name}</p>
+        <p>My age is {this.props.age} </p>
+        <button onClick={this.changeColor}>Change Color</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Person name="Shyam" age="23" />, document.getElementById('root'));
