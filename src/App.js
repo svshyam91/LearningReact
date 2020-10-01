8e5664ce50605cdc53242548056cdb4ce9f4ac0c
svshyam91 <svshyam111@gmail.com>
2020-10-01 12:54:08 +0530

    Example of Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  
+// This program demostrate conditional rendering 
+
+class Person extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: props.name,
+      yearOfBirth: props.yearOfBirth,
+      gender: props.gender,
+    }
+  }
+
+  calculateAge() {
+    return 2020-this.state.yearOfBirth;
+  }
+
+  render() {
+    return(
+      <div className='person'>
+        <p>Name: {this.state.name}</p>
+        <p>Age: {this.calculateAge()}</p>
+        <p>Gender: {this.state.gender}</p>
+      </div>
+    )
+  }
 }
 
-export default App;
+class Family extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      togglePerson: false
+    }
+  }
+  togglePersonHandler() {
+    this.setState({togglePerson: !this.state.togglePerson})
+  }
+  render() {
+    return(
+      <div>
+        <button onClick={this.togglePersonHandler.bind(this)}>Show/Hide</button> 
+        { this.state.togglePerson ? 
+          <div>
+            <Person name="Shyam" yearOfBirth="1997" gender="male" />
+            <Person name="Vishwanath" yearOfBirth="1952" gender="male" />
+            <Person name="Indrawati" yearOfBirth="1960" gender="female" />
+            <Person name="Suneel" yearOfBirth="1993" gender="male" />
+          </div>
+         : null
+        }
+      </div>
+    )
+  }
+}
+
+export default Family;
--- src/index.js
+++ src/index.js
@@ -1,39 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-// Clock using props
-class Person extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'Shyam',
-      age: 23
-    }
-  }
+import Family from "./App";
 
-  clickHandler(fnName, lastName) {
-    // In this function, 'this' keyword will not work without 'bind()' because here this refers to the element that triggered click event 
-    // which is ButtonElement
-
-    console.log('First name: '+fnName+' Last Name: '+lastName);
-    console.log('This is '+this.state.name);
-  }
-
-  linkHandler = (e) => {
-    e.preventDefault();
-    console.log('You have clicked on a link.');
-    console.log('linkHandler this is'+this.state.name);
-  }
-
-  render() {
-    return(
-      <div>
-        <button onClick={this.clickHandler.bind(this, 'Harkesh', 'Verma')}>Click me!!</button>
-        <a href="#" onClick={this.linkHandler}>Open Google</a>
-      </div>
-    )
-  }
-
-} 
-
-ReactDOM.render(<Person />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <Family />
+  </React.StrictMode>,
+  rootElement
+);
