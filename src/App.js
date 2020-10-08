53a8f4908e986aa8376ecf72ccbdaabee09c8f8b
svshyam91 <svshyam111@gmail.com>
2020-10-08 15:18:29 +0530

    Form with multiple elements with common handler

--- src/App.js
+++ src/App.js
@@ -1,46 +1,87 @@
 import React from "react";
 import "./App.css";
 
-// Form using Select tag 
+// Form with multiple elements with common handler
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      favColor: '',
-    }
-    this.favColorHandler = this.favColorHandler.bind(this);
+      username: "",
+      age: "",
+      pass: "",
+      comment: "",
+      favColor: ""
+    };
+    this.changeHandler = this.changeHandler.bind(this);
     this.submitHandler = this.submitHandler.bind(this);
   }
 
-  favColorHandler(event) {
-    this.setState({favColor: event.target.value});
+  changeHandler(event) {
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   submitHandler(event) {
-    alert('User\'s Fav. Color: '+this.state.favColor);
+    alert("User's Fav. Color: " + this.state.favColor);
     event.preventDefault();
   }
 
   render() {
-   return(
-    <div>
-      <form onSubmit={this.submitHandler}>
-        <label>
-          Select your favourite Color:
-          <select value={this.state.favColor} onChange={this.favColorHandler}>
-            <option>Red</option>
-            <option>Green</option>
-            <option>Blue</option>
-            <option>White</option>
-            <option>Black</option>
-            <option>Indigo</option>
-          </select>
-        </label>
-        <input type='submit' value='submit' />
-      </form>
-    </div>
-   )
+    return (
+      <div>
+        <form onSubmit={this.submitHandler}>
+          <label>
+            Username:
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.changeHandler}
+            />
+          </label>
+          <br />
+
+          <label>
+            Age:
+            <input
+              type="number"
+              name="age"
+              value={this.state.age}
+              onChange={this.changeHandler}
+            />
+          </label>
+          <br />
+
+          <label>
+            Password:
+            <input
+              type="password"
+              name="pass"
+              value={this.state.pass}
+              onChange={this.changeHandler}
+            />
+          </label>
+          <br />
+
+          <label>
+            Select your favourite Color:
+            <select
+              name="favColor"
+              value={this.state.favColor}
+              onChange={this.changeHandler}
+            >
+              <option>Red</option>
+              <option>Green</option>
+              <option>Blue</option>
+              <option>White</option>
+              <option>Black</option>
+              <option>Indigo</option>
+            </select>
+          </label>
+          <input type="submit" value="submit" />
+        </form>
+      </div>
+    );
   }
 }
 
